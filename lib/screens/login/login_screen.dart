ea7fe3e245ef74377f8503ffcd61df620f5ff7a2
raycbn <74503112+raycbn@users.noreply.github.com>
2026-07-22 00:16:00 +0200

    Add LoginScreen widget for user login interface

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
