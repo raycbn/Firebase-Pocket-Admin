f6cb07ff6e2bf9674907ec41b275f8d5a6458c59
raycbn <74503112+raycbn@users.noreply.github.com>
2026-07-22 00:14:49 +0200

    Add AppTheme class with light and dark themes

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static const Color primaryColor = Color(0xFFFF6F00);
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: primaryColor,
+      brightness: Brightness.light,
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: primaryColor,
+      brightness: Brightness.dark,
+    );
+  }
+}
