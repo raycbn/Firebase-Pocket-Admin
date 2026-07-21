3ea369b404f796bf58db6d35dd0970a71a07ddf0
raycbn <74503112+raycbn@users.noreply.github.com>
2026-07-22 00:14:32 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+import 'config/routes.dart';
+import 'theme/app_theme.dart';
+
+class FireAdminApp extends StatelessWidget {
+  const FireAdminApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FireAdmin',
+      debugShowCheckedModeBanner: false,
+
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+
+      initialRoute: AppRoutes.splash,
+
+      routes: AppRoutes.routes,
+    );
+  }
+}
